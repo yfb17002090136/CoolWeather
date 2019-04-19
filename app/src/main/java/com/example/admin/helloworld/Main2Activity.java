ae55e77d9fe17415b7c3edc30935e846980f1ba5
11231 <13213>
2019-04-19 08:58:57 +0800

    4.19

--- app/src/main/java/com/example/admin/helloworld/Main2Activity.java
+++ app/src/main/java/com/example/admin/helloworld/Main2Activity.java
@@ -3,7 +3,9 @@ package com.example.admin.helloworld;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -14,6 +16,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -21,7 +25,10 @@ import okhttp3.Response;
 
 public class Main2Activity extends AppCompatActivity {
 
-    private  String[] data={"北京","浙江","安徽"};
+
+    private List<String> data2=new ArrayList();
+    private int[] pids = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    private  String[] data={"北京","浙江","安徽","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
     private TextView textView;
     private Button button;
     private ListView listview;
@@ -32,8 +39,18 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         this.textView = (TextView)findViewById(R.id.textView);
         this.listview=(ListView)findViewById(R.id.listview);
+
         ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
         listview.setAdapter(adapter);
+        this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("点击了哪一个",""+position+":"+Main2Activity.this.pids[position]+":"+Main2Activity.this.data[position]);
+                Intent intent=new Intent(Main2Activity.this,MainActivity.class);
+                intent.putExtra("pid",Main2Activity.this.pids[position]);
+                startActivity(intent);
+            }
+        });
 
         this.button=(Button)findViewById(R.id.button);
         this.button.setOnClickListener(new View.OnClickListener() {
@@ -47,7 +64,8 @@ public class Main2Activity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException{
                 final String responseText = response.body().string();
-                String[] result=parseJSONWithJSONObject(responseText);
+                 parseJSONWithJSONObject(responseText);
+
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -55,6 +73,7 @@ public class Main2Activity extends AppCompatActivity {
                     }
                 });
             }
+
             @Override
             public void onFailure(Call call, IOException e) {
 
@@ -62,19 +81,19 @@ public class Main2Activity extends AppCompatActivity {
         });
     }
 
-    private String[] parseJSONWithJSONObject(String responseText) {
+    private void  parseJSONWithJSONObject(String responseText) {
         JSONArray jsonArray= null;
         try {
             jsonArray = new JSONArray(responseText);
             String[] result=new String[jsonArray.length()];
             for(int i=0;i<jsonArray.length();i++){
-                JSONObject jsonObject=jsonArray.getJSONObject(i);
-                result[i]=jsonObject.getString("name");
+                JSONObject jsonObject=null;
+                jsonObject = jsonArray.getJSONObject(i);
+                this.data[i]=jsonObject.getString("name");
+                this.pids[i]=jsonObject.getInt("id");
             }
-            return result;
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return null;
     }
 }
